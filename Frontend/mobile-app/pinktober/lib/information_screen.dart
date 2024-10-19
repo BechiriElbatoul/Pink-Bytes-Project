4bf2d5e07ce5aeb7033d924e5a28c27e2c54bcd5
AmineLadrem <102830544+AmineLadrem@users.noreply.github.com>
2024-10-19 22:53:37 +0200

    feat : backend

--- Frontend/mobile-app/pinktober/lib/information_screen.dart
+++ Frontend/mobile-app/pinktober/lib/information_screen.dart
@@ -1,262 +1,156 @@
 import 'package:flutter/material.dart';
 
-class InformationScreen extends StatefulWidget {
-  const InformationScreen({super.key});
-
-  @override
-  _InformationScreenState createState() => _InformationScreenState();
-}
-
-class _InformationScreenState extends State<InformationScreen> {
-  String? _gender;
-  DateTime? _selectedDate;
-  bool _hasFamilyHistory = false;
-
+class InformationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset:
-          false, // Prevent layout from resizing when keyboard opens
-      body: SafeArea(
-        // Wrap the Stack in a SafeArea
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return Stack(
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SingleChildScrollView(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 32.0, vertical: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 40),
-                      const Text(
-                        'Welcome Amira ~',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Quicksand',
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      const Text(
-                        'Few more informations we need to know about you',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Quicksand',
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      _buildLabel('Gender'),
-                      Row(
-                        children: [
-                          Row(
-                            children: [
-                              Radio<String>(
-                                value: 'Male',
-                                groupValue: _gender,
-                                activeColor: Colors.pink,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _gender = value;
-                                  });
-                                },
-                              ),
-                              const Text('Male',
-                                  style: TextStyle(
-                                      fontFamily: 'Quicksand', fontSize: 14)),
-                            ],
-                          ),
-                          const SizedBox(width: 16),
-                          Row(
-                            children: [
-                              Radio<String>(
-                                value: 'Female',
-                                groupValue: _gender,
-                                activeColor: Colors.pink,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _gender = value;
-                                  });
-                                },
-                              ),
-                              const Text('Female',
-                                  style: TextStyle(
-                                      fontFamily: 'Quicksand', fontSize: 14)),
-                            ],
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 16),
-                      _buildLabel('Date of Birth'),
-                      GestureDetector(
-                        onTap: _selectDate,
-                        child: AbsorbPointer(
-                          child: _buildTextField(
-                            hint: _selectedDate != null
-                                ? "${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}"
-                                : 'Select your birth date',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      _buildLabel('Country'),
-                      _buildTextField(hint: 'Type your country'),
-                      const SizedBox(height: 16),
-                      Center(
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Text(
-                              'Family history of breast cancer',
-                              style: TextStyle(
-                                fontFamily: 'Quicksand',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  _hasFamilyHistory = !_hasFamilyHistory;
-                                });
-                              },
-                              child: Container(
-                                width: 24,
-                                height: 24,
-                                decoration: BoxDecoration(
-                                  border:
-                                      Border.all(color: Colors.pink, width: 2),
-                                  borderRadius: BorderRadius.circular(4),
-                                  color: _hasFamilyHistory
-                                      ? Colors.pink
-                                      : Colors.white,
-                                ),
-                                child: _hasFamilyHistory
-                                    ? const Icon(
-                                        Icons.check,
-                                        size: 16,
-                                        color: Colors.white,
-                                      )
-                                    : null,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Center(
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.pink[100],
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 40, vertical: 12),
-                          ),
-                          onPressed: () {},
-                          child: const Text(
-                            'Finalize',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: 'Quicksand',
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 140),
-                    ],
+                SizedBox(height: 80),
+                Text(
+                  "Welcome Amira ~",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Quicksand',
                   ),
                 ),
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  child: Image.asset(
-                    'assets/images/sign_up.png',
-                    height: 180,
-                    fit: BoxFit.cover,
+                SizedBox(height: 10),
+                Text(
+                  "Few more informations we\nneed to know about you",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Quicksand',
+                  ),
+                ),
+                SizedBox(height: 40),
+                // Gender Dropdown
+                Text(
+                  "Gender",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Quicksand',
+                  ),
+                ),
+                SizedBox(height: 8),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.pinkAccent),
+                    ),
+                  ),
+                  items: ['Male', 'Female', 'Other'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                ),
+                SizedBox(height: 20),
+                // Date of Birth Input
+                Text(
+                  "Date of birth",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Quicksand',
+                  ),
+                ),
+                SizedBox(height: 8),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Input Default",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.pinkAccent),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                // Country Dropdown
+                Text(
+                  "Country",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Quicksand',
+                  ),
+                ),
+                SizedBox(height: 8),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.pinkAccent),
+                    ),
+                  ),
+                  items: ['Switzerland', 'Germany', 'France', 'Other']
+                      .map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                ),
+                SizedBox(height: 20),
+                // Family History of Breast Cancer Checkbox
+                Row(
+                  children: [
+                    Checkbox(
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                    Text(
+                      "Family History of Breast Cancer",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Quicksand',
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                // Finalize Button
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pinkAccent,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "FINALIZE",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Quicksand',
+                      ),
+                    ),
                   ),
                 ),
               ],
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  Widget _buildLabel(String label) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 6.0),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          label,
-          style: const TextStyle(
-            fontSize: 14,
-            color: Colors.black87,
-            fontFamily: 'Quicksand',
+            ),
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTextField({required String hint}) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: hint,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.pinkAccent),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.pinkAccent),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.pink, width: 2),
-        ),
-        fillColor: Colors.white,
-        filled: true,
-        contentPadding:
-            const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      ),
-      style: const TextStyle(
-        fontFamily: 'Quicksand',
-        fontSize: 14,
-      ),
-    );
-  }
-
-  Future<void> _selectDate() async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate ?? DateTime.now(),
-      firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
-      builder: (BuildContext context, Widget? child) {
-        return Theme(
-          data: ThemeData.light().copyWith(
-            primaryColor: Colors.pink,
-            hintColor: Colors.pink,
-            colorScheme: ColorScheme.light(primary: Colors.pink),
-            buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Image.asset(
+              'assets/images/sign_up.png',
+              height: 200,
+            ),
           ),
-          child: child ?? const Text(''),
-        );
-      },
+        ],
+      ),
     );
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-      });
-    }
   }
 }
